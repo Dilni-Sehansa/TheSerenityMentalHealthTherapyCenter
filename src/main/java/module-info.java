782eccea00f9152301caa5d2ad9029a1097b7b3f
Dilni Sehansa <dilnisehansa29@gmail.com>
2026-05-06 11:36:40 +0530

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lk.ijse.theserenitymentalhealththerapycenter {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lk.ijse.theserenitymentalhealththerapycenter to javafx.fxml;
+    exports lk.ijse.theserenitymentalhealththerapycenter;
+}
